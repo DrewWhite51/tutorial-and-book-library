6e725c8f061584151152978746153a229f172cc5
Drew White <drew.m.white@gmail.com>
2022-08-02 19:51:04 -0500

    Learning about writing unit tests.

--- unit-tests-example/developer.py
+++ unit-tests-example/developer.py
@@ -1,20 +1,28 @@
+import requests
 
 class Developer:
 
     salary = 100_000
 
     def __init__(self, first_name, last_name, fav_language):
-        self.first_name = first_name,
+        self.first_name = first_name
         self.last_name = last_name
         self.fav_language = fav_language
 
     @property
     def email(self):
-        return '{}.{}@gmail.com'.format(self.first_name, self.last_name)
+        return f'{self.first_name}.{self.last_name}@gmail.com'
 
     @property
     def fullname(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
     def give_raise(self):
-        self.salary = self.salary + 10_000
+        return self.salary + 10_000
+
+    def scrape_webpage(self):
+        response = requests.get("https://outlook.live.com/mail/0/")
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response'
--- unit-tests-example/developer_test.py
+++ unit-tests-example/developer_test.py
@@ -1,16 +1,56 @@
 import unittest
-import developer
+from unittest.mock import patch
+from developer import Developer
 
 # Write tests here
 
 
 class TestDeveloper(unittest.TestCase):
+    # These are for doing the setup and tear down one time for all tests
+    # This is more often for things that are data intensive and only need to be set up once
+    # @classmethod
+    # def setUpClass(cls):
+    #     print('setupClass')
+    #
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print('teardownClass')
+
+    # This happens at the beginning of every test.
+    def setUp(self):
+        self.dev1 = Developer('Drew', 'White', 'Python')
+        self.dev2 = Developer('Rich', 'White', 'PHP')
+
+    # This happens at the end of every test
+    # Handy for creating objects such as DATABASES and files created during the test
+    def tearDown(self):
+        pass
 
     def test_email(self):
-        pass
+
+        self.assertEqual(self.dev1.email, 'Drew.White@gmail.com')
+        self.assertEqual(self.dev2.email, 'Rich.White@gmail.com')
+
+        self.dev1.last_name = 'Beast'
+        self.dev2.last_name = 'Savage'
+
+        self.assertEqual(self.dev1.email, 'Drew.Beast@gmail.com')
+        self.assertEqual(self.dev2.email, 'Rich.Savage@gmail.com')
 
     def test_fullname(self):
-        pass
+
+        self.assertEqual(self.dev1.fullname, 'Drew White')
+        self.assertEqual(self.dev2.fullname, 'Rich White')
 
     def test_give_raise(self):
-        pass
+
+        self.assertEqual(self.dev1.give_raise(), 110_000)
+        self.assertEqual(self.dev2.give_raise(), 110_000)
+
+        self.dev1.salary = 150_000
+        self.dev2.salary = 170_000
+
+        self.assertEqual(self.dev1.give_raise(), 160_000)
+        self.assertEqual(self.dev2.give_raise(), 180_000)
+
+
